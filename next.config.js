d885c463738728da2b6c6d5c5473e2896fc42bff
Jacquiline Damasco <jacqdmsc@gmail.com>
2026-03-21 23:08:34 +0800

    add all Spotify image CDN domains to next.config.js

--- next.config.js
+++ next.config.js
@@ -4,6 +4,13 @@ const nextConfig = {
     remotePatterns: [
       { protocol: 'https', hostname: 'i.scdn.co' },
       { protocol: 'https', hostname: 'mosaic.scdn.co' },
+      { protocol: 'https', hostname: 'image-cdn-ak.spotifycdn.com' },
+      { protocol: 'https', hostname: 'image-cdn-fa.spotifycdn.com' },
+      { protocol: 'https', hostname: 'seeded-session-images.scdn.co' },
+      { protocol: 'https', hostname: 'blend-playlist-covers.spotifycdn.com' },
+      { protocol: 'https', hostname: 'thisis-images.spotifycdn.com' },
+      { protocol: 'https', hostname: '*.scdn.co' },
+      { protocol: 'https', hostname: '*.spotifycdn.com' },
     ],
   },
 }
